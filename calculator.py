b015ed946b168b8cd22c88e37d47526a08c34c86
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-04 14:58:57 -0700

    Added try and except to account for user error.

--- calculator.py
+++ calculator.py
@@ -6,48 +6,64 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+operators = ["+", "-", "*", "/", "square", "cube", "pow", "mod", "x+", "cubes+", "q"]
+
 
 while True:
     user_input = raw_input('> ')
     input_tokens = user_input.split(' ')
 
-    if input_tokens[0] == '+':
-        add_result = add(float(input_tokens[1]), float(input_tokens[2]))
-        print add_result
+    if input_tokens[0] in operators:
 
-    if input_tokens[0] == '-':
-        sub_result = subtract(float(input_tokens[1]), float(input_tokens[2]))
-        print sub_result
+        try:
+            
+            if input_tokens[0] == '+':
+                add_result = add(float(input_tokens[1]), float(input_tokens[2]))
+                print add_result
 
-    if input_tokens[0] == '*':
-        mult_result = multiply(float(input_tokens[1]), float(input_tokens[2]))
-        print mult_result
+            if input_tokens[0] == '-':
+                sub_result = subtract(float(input_tokens[1]), float(input_tokens[2]))
+                print sub_result
 
-    if input_tokens[0] == '/':
-        div_result = divide(float(input_tokens[1]), float(input_tokens[2]))
-        print div_result
+            if input_tokens[0] == '*':
+                mult_result = multiply(float(input_tokens[1]), float(input_tokens[2]))
+                print mult_result
 
-    if input_tokens[0] == 'square':
-        sq_result = square(float(input_tokens[1]))
-        print sq_result
+            if input_tokens[0] == '/':
+                div_result = divide(float(input_tokens[1]), float(input_tokens[2]))
+                print div_result
 
-    if input_tokens[0] == 'cube':
-        cube_result = cube(float(input_tokens[1]))
-        print cube_result
+            if input_tokens[0] == 'square':
+                sq_result = square(float(input_tokens[1]))
+                print sq_result
 
-    if input_tokens[0] == 'pow':
-        pow_result = power(float(input_tokens[1]), float(input_tokens[2]))
-        print pow_result
+            if input_tokens[0] == 'cube':
+                cube_result = cube(float(input_tokens[1]))
+                print cube_result
 
-    if input_tokens[0] == 'mod':
-        mod_result = mod(float(input_tokens[1]), float(input_tokens[2]))
-        print mod_result
+            if input_tokens[0] == 'pow':
+                pow_result = power(float(input_tokens[1]), float(input_tokens[2]))
+                print pow_result
 
-    if input_tokens[0] == 'x+':
-        add_mult_result = add_mult(float(input_tokens[1]), float(input_tokens[2]), float(input_tokens[3]))
-        print add_mult_result
+            if input_tokens[0] == 'mod':
+                mod_result = mod(float(input_tokens[1]), float(input_tokens[2]))
+                print mod_result
 
-    if input_tokens[0] == 'cubes+':
-        add_cubes_result = add_cubes(float(input_tokens[1]), float(input_tokens[2]))
-        print add_cubes_result
+            if input_tokens[0] == 'x+':
+                add_mult_result = add_mult(float(input_tokens[1]), float(input_tokens[2]), float(input_tokens[3]))
+                print add_mult_result
 
+            if input_tokens[0] == 'cubes+':
+                add_cubes_result = add_cubes(float(input_tokens[1]), float(input_tokens[2]))
+                print add_cubes_result
+
+            if user_input == "q":
+                print "You will exit."
+                break
+
+        except ValueError:
+            print "Invalid input, numbers must be entered for calculations."
+            continue
+      
+    else:
+        print "Invalid input, please enter an operator or q to quit."
